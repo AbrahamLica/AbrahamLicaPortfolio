8a900398de50ac6cddc16e0110084df22ca22f4e
Abraham Melquisedeque Pereira Licá <103894033+AbrahamLica@users.noreply.github.com>
2025-01-11 22:32:08 -0300

    more
    more

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
+import Header from './components/header/header';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
 
+  console.log('teste');
+  
+
   return (
     <>
-      <p></p>
+      <Header></Header>
     </>
   );
 }
---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import * as C from "./styles"
+
+export const Header = () => {
+    return(
+        <div>
+            <C.button>testead</C.button>
+        </div>
+    )
+}
+
+export default Header
